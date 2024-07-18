4ac7776b3faeecce31f3003b117fe90ce034afb0
daditsan <100355396+daditsan@users.noreply.github.com>
2024-07-18 10:13:51 +0700

    wip

--- client/src/page/LitsProduct.jsx
+++ client/src/page/LitsProduct.jsx
@@ -2,10 +2,8 @@ import { Link } from "react-router-dom"
 
 function ListProduct(params) {
     return (
-        <div>
-
-
-            <div className="card bg-base-100 w-72 shadow-xl p-6 ms-4">
+        <div className="flex" style={{ height: '100vh', width: ''}}>
+            <div className="card bg-base-100 w-72 shadow-xl p-6 ms-4 mt-4" style={{ height: "400px"}}>
                 <figure>
                     <img
                         src="https://img.antaranews.com/cache/1200x800/2022/06/07/antarafoto-2022-06-06t190737z_440389118_rc2imu94t7um_rtrmadp_3_apple-developer.jpg.webp"
@@ -19,6 +17,8 @@ function ListProduct(params) {
                     </div>
                 </div>
             </div>
+
+            {/* disini untuk nambahin card product baru, kalo ada */}
         </div>
     )
 }
